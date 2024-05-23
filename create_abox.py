01a934d719ad0aa901ab03ec90e968bbf17ca55f
darryl-abraham <darryl.br@gmail.com>
2024-05-23 07:01:51 +0200

    added create_abox.py with systematic triple creation, updated csvs and data_augmentation.ipynb (added data prep)

---
+++ create_abox.py
@@ -0,0 +1,98 @@
+from rdflib import Graph, Namespace, Literal, URIRef
+from rdflib.namespace import RDF, RDFS, XSD
+import pandas as pd
+
+# Create a Graph
+abox = Graph()
+
+# Define Namespaces
+url = Namespace("http://sdmlab2.org/")
+abox.bind("url", url)
+
+# add instance data from .csv files
+
+author = pd.read_csv('author.csv')
+for index, row in author.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.hasName, Literal(row['name'], datatype=XSD.string)))
+
+company_affiliation = pd.read_csv('AUTHOR_AFFILIATED_COMPANY.csv')
+for index, row in company_affiliation.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.worksForCompany, URIRef(url + row['Company_ID'])))
+
+company = pd.read_csv('Company.csv')
+for index, row in company.iterrows():
+    abox.add((URIRef(url + row['Company_ID']), url.companyName, row['name']))
+
+university_affiliation = pd.read_csv('AUTHOR_AFFILIATED_UNIVERSITY.csv')
+for index, row in university_affiliation.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.worksForUniversity, URIRef(url + row['University_ID'])))
+
+university = pd.read_csv('University.csv')
+for index, row in university.iterrows():
+    abox.add((URIRef(url + row['University_ID']), url.universityName, row['name']))
+
+authored = pd.read_csv('AUTHORED.csv')
+for index, row in authored.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.wrotePaper, URIRef(url + row['Paper_ID'])))
+
+cites = pd.read_csv('CITES.csv')
+for index, row in cites.iterrows():
+    abox.add((URIRef(url + row['Cited_Paper_ID']), url.citedBy, URIRef(url + row['Citing_Paper_ID'])))
+
+paper = pd.read_csv('Paper.csv')
+for index, row in paper.iterrows():
+    abox.add((URIRef(url + row['Paper_ID']), url.hasTitle, row['title']))
+    abox.add((URIRef(url + row['Paper_ID']), url.hasAbstract, row['abstract']))
+    abox.add((URIRef(url + row['Author_ID']), url.correspondingAuthor, (URIRef(url + row['Paper_ID']))))
+
+review = pd.read_csv('REVIEWED.csv')
+for index, row in review.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.wroteReview, URIRef(url + row['Review_ID'])))
+    abox.add((URIRef(url + row['Review_ID']), url.reviewOf, URIRef(url + row['Paper_ID'])))
+    abox.add((URIRef(url + row['Review_ID']), url.hasText, row['text']))
+
+reviewers = pd.read_csv('Reviewer.csv')
+for index, row in reviewers.iterrows():
+    abox.add((URIRef(url + row['Author_ID']), url.relevantAuthorOf, URIRef(url + row['relevantAuthorOf'])))
+
+keyword = pd.read_csv('Keyword.csv')
+for index, row in keyword.iterrows():
+    abox.add((URIRef(url + row['Keyword_ID']), url.isWord, row['word']))
+    abox.add((URIRef(url + row['Keyword_ID']), url.partOfTopic, (URIRef(url + row['Topic_ID']))))
+    abox.add((URIRef(url + row['Topic_ID']), url.isAbout, row['topic']))
+
+haskeywords = pd.read_csv('IS_ABOUT.csv')
+for index, row in haskeywords.iterrows():
+    abox.add((URIRef(url + row['Paper_ID']), url.hasKeyword, URIRef(url + row['Keyword_ID'])))
+
+publishedIn = pd.read_csv('PUBLISHED_IN.csv')
+for index, row in publishedIn.iterrows():
+    abox.add((URIRef(url + row['Paper_ID']), url.publishedIn, URIRef(url + row['Volume_ID'])))
+
+volume = pd.read_csv('Volume.csv')
+for index, row in volume.iterrows():
+    abox.add((URIRef(url + row['Volume_ID']), url.volumeName, row['name']))
+    abox.add((URIRef(url + row['Volume_ID']), url.publicationYear, row['year']))
+    abox.add((URIRef(url + row['Volume_ID']), url.publishedByJournal, (URIRef(url + row['Journal_ID']))))
+
+journal = pd.read_csv('Journal.csv')
+for index, row in journal.iterrows():
+    abox.add((URIRef(url + row['Journal_ID']), url.journalName, row['name']))
+
+presentedAt = pd.read_csv('PRESENTED_AT.csv')
+for index, row in presentedAt.iterrows():
+    abox.add((URIRef(url + row['Paper_ID']), url.publishedInEdition, URIRef(url + row['Conference_ID'])))
+
+proceeding = pd.read_csv('Edition.csv')
+for index, row in proceeding.iterrows():
+    abox.add((URIRef(url + row['Edition_ID']), url.publishedByConfWork, (URIRef(url + row['Conference_ID']))))
+    abox.add((URIRef(url + row['Edition_ID']), url.heldIn, row['city']))
+    abox.add((URIRef(url + row['Edition_ID']), url.publicationYear, row['year']))
+
+confwork = pd.read_csv('Conference.csv')
+for index, row in confwork.iterrows():
+    abox.add((URIRef(url + row['Conference_ID']), url.conferenceName, row['name']))
+
+# Serialize to ttl and save
+turtle_data = abox.serialize(destination='abox.ttl', format='turtle')
+
--- create_tbox.py
+++ create_tbox.py
@@ -1,21 +1,128 @@
-from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib import Graph, Namespace, URIRef, Literal, XSD
 from rdflib.namespace import RDF, RDFS
 
 # Create a Graph
-g = Graph()
+tbox = Graph()
 
 # Define Namespaces
-ex = Namespace("http://example.org/")
-g.bind("ex", ex)
+url = Namespace("http://sdmlab2.org/")
+tbox.bind("url", url)
 
 # Add Ontology Data
-g.add((ex.Author, RDF.type, RDFS.Class))
-g.add((ex.hasName, RDF.type, RDF.Property))
-g.add((ex.Author, ex.hasName, RDFS.Literal))
 
-# Serialize to Turtle
-turtle_data = g.serialize(format='turtle')
+# Author
+# Author name
+tbox.add((url.hasName, RDFS.domain, url.Author))
+tbox.add((url.hasName, RDFS.range, XSD.Literal))
+# Author works for
+tbox.add((url.worksForCompany, RDFS.domain, url.Author))
+tbox.add((url.worksForCompany, RDFS.range, url.Company))
+tbox.add((url.worksForUniversity, RDFS.domain, url.Author))
+tbox.add((url.worksForUniversity, RDFS.range, url.University))
+# Author authored
+tbox.add((url.Authored, RDFS.domain, url.Author))
+tbox.add((url.wrotePaper, RDFS.subPropertyOf, url.Authored))
+tbox.add((url.wrotePaper, RDFS.range, url.Paper))
+tbox.add((url.wroteReview, RDFS.subPropertyOf, url.Authored))
+tbox.add((url.wroteReview, RDFS.range, url.Review))
+tbox.add((url.correspondingAuthor, RDFS.subPropertyOf, url.Authored))
+tbox.add((url.correspondingAuthor, RDFS.range, url.Paper))
+# Author relevant in
+tbox.add((url.relevantAuthorOf, RDFS.domain, url.Author))
+tbox.add((url.relevantAuthorOf, RDFS.range, url.ConfWork))
+tbox.add((url.relevantAuthorOf, RDFS.range, url.Journal))
 
-# Write to TBox file
-with open('example.ttl', 'wb') as f:
-    f.write(turtle_data.encode())
+# Organization
+# Company
+tbox.add((url.worksForCompany, RDFS.subpropertyOf, url.worksFor))
+tbox.add((url.Company, RDFS.subClassOf, url.Organization))
+tbox.add((url.companyName, RDFS.subPropertyOf, url.orgName))
+tbox.add((url.companyName, RDFS.domain, url.Company))
+tbox.add((url.companyName, RDFS.range, XSD.Literal))
+# University
+tbox.add((url.worksForUniversity, RDFS.subpropertyOf, url.worksFor))
+tbox.add((url.University, RDFS.subClassOf, url.Organization))
+tbox.add((url.universityName, RDFS.subPropertyOf, url.orgName))
+tbox.add((url.universityName, RDFS.domain, url.University))
+tbox.add((url.universityName, RDFS.range, XSD.Literal))
+
+# Review
+# Review of
+tbox.add((url.reviewOf, RDFS.domain, url.Review))
+tbox.add((url.reviewOf, RDFS.range, url.Paper))
+# Review text
+tbox.add((url.hasText, RDFS.domain, url.Review))
+tbox.add((url.hasText, RDFS.range, XSD.Literal))
+
+# Paper
+# Paper title
+tbox.add((url.hasTitle, RDFS.domain, url.Paper))
+tbox.add((url.hasTitle, RDFS.range, XSD.Literal))
+# Paper abstract
+tbox.add((url.hasAbstract, RDFS.domain, url.Paper))
+tbox.add((url.hasAbstract, RDFS.range, XSD.Literal))
+# Paper keywords + TOPICS
+tbox.add((url.hasKeyword, RDFS.domain, url.Paper))
+tbox.add((url.hasKeyword, RDFS.range, url.Keyword))
+tbox.add((url.isWord, RDFS.domain, url.Keyword))
+tbox.add((url.isWord, RDFS.range, XSD.Literal))
+tbox.add((url.partOfTopic, RDFS.domain, url.Keyword))
+tbox.add((url.partOfTopic, RDFS.range, url.Topic))
+tbox.add((url.isAbout, RDFS.domain, url.Topic))
+tbox.add((url.isAbout, RDFS.range, XSD.Literal))
+# Paper citation
+tbox.add((url.citedBy, RDFS.domain, url.Paper))
+tbox.add((url.citedBy, RDFS.range, url.Paper))
+# Paper published in
+tbox.add((url.publishedIn, RDFS.domain, url.Paper))
+tbox.add((url.publishedIn, RDFS.range, url.Collection))
+tbox.add((url.publishedInProceeding, RDFS.subPropertyOf, url.publishedIn))
+tbox.add((url.publishedInProceeding, RDFS.range, url.Proceeding))
+tbox.add((url.publishedInVolume, RDFS.subPropertyOf, url.publishedIn))
+tbox.add((url.publishedInVolume, RDFS.range, url.Volume))
+# ??
+tbox.add((url.Proceeding, RDFS.subClassOf, url.Collection))
+tbox.add((url.Volume, RDFS.subClassOf, url.Collection))
+
+# Published by
+tbox.add((url.publishedBy, RDFS.domain, url.Collection))
+tbox.add((url.publishedBy, RDFS.range, url.Publication))
+tbox.add((url.publishedByConfWork, RDFS.subPropertyOf, url.publishedBy))
+tbox.add((url.PublishedByConfWork, RDFS.domain, url.Proceeding))
+tbox.add((url.publishedByConfWork, RDFS.range, url.ConfWork))
+tbox.add((url.publishedByJournal, RDFS.subPropertyOf, url.publishedBy))
+tbox.add((url.publishedByJournal, RDFS.domain, url.Volume))
+tbox.add((url.publishedByJournal, RDFS.range, url.Journal))
+# ??
+tbox.add((url.ConfWork, RDFS.subClassOf, url.Publication))
+tbox.add((url.Journal, RDFS.subClassOf, url.Publication))
+
+# Volume
+# Volume name
+tbox.add((url.volumeName, RDFS.domain, url.Volume))
+tbox.add((url.volumeName, RDFS.range, XSD.Literal))
+# Volume date
+tbox.add((url.publicationYear, RDFS.domain, url.Volume))
+tbox.add((url.publicationYear, RDFS.range, XSD.Integer))
+
+# Proceeding
+# Proceeding date
+tbox.add((url.publicationYear, RDFS.domain, url.Proceeding))
+tbox.add((url.publicationYear, RDFS.range, XSD.Integer))
+# Proceeding city
+tbox.add((url.heldIn, RDFS.domain, url.Proceeding))
+tbox.add((url.heldIn, RDFS.range, XSD.Literal))
+
+# Conference
+# Conference name
+tbox.add((url.conferenceName, RDFS.domain, url.ConfWork))
+tbox.add((url.conferenceName, RDFS.range, XSD.Literal))
+
+# Journal
+# Journal name
+tbox.add((url.journalName, RDFS.domain, url.Journal))
+tbox.add((url.journalName, RDFS.range, XSD.Literal))
+
+# Serialize to .ttl and save
+turtle_data = tbox.serialize(destination='tbox.ttl', format='turtle')
+
